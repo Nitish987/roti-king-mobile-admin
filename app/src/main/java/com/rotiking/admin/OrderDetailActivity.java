c9a196327ee6b92a1bdf2e725ea27af407f5cab8
Nitish Kumar <nk98721000@gmail.com>
2022-09-24 18:33:56 +0530

    order details activity

---
+++ app/src/main/java/com/rotiking/admin/OrderDetailActivity.java
@@ -0,0 +1,211 @@
+package com.rotiking.admin;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.rotiking.admin.adapter.CheckoutCartItemRecyclerAdapter;
+import com.rotiking.admin.models.CartItem;
+import com.rotiking.admin.models.CheckoutCartItem;
+import com.rotiking.admin.models.Order;
+import com.rotiking.admin.utils.DateParser;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderDetailActivity extends AppCompatActivity {
+    private RecyclerView orderItemRV;
+    private ImageButton closeBtn;
+    private TextView itemsTxt, orderNumberTxt, totalCartPriceTxt, deliveryCartPriceTxt, totalPayableTxt, nameTxt, phoneTxt, addressTxt, agentNameTxt, agentPhoneTxt, timeTxt, paymentMethodTxt;
+    private AppCompatButton cancelOrderBtn, acceptOrderBtn;
+    private LinearProgressIndicator orderStateIndicator;
+    private TextView orderedStateTxt, orderedState, cookingState, dispatchedState, onWayState, deliveredState;
+    private LinearLayout deliveryAgentDesk;
+
+    private String orderId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+
+        orderId = getIntent().getStringExtra("ORDER");
+
+        closeBtn = findViewById(R.id.close);
+        itemsTxt = findViewById(R.id.items);
+        orderNumberTxt = findViewById(R.id.order_number);
+        totalCartPriceTxt = findViewById(R.id.total_price);
+        deliveryCartPriceTxt = findViewById(R.id.delivery_price);
+        totalPayableTxt = findViewById(R.id.payable_price);
+        cancelOrderBtn = findViewById(R.id.cancel);
+        nameTxt = findViewById(R.id.name);
+        phoneTxt = findViewById(R.id.phone);
+        addressTxt = findViewById(R.id.address);
+        agentNameTxt = findViewById(R.id.agent_name);
+        agentPhoneTxt = findViewById(R.id.agent_phone);
+        deliveryAgentDesk = findViewById(R.id.delivery_agent_desk);
+        timeTxt = findViewById(R.id.time);
+        paymentMethodTxt = findViewById(R.id.payment_method);
+        orderStateIndicator = findViewById(R.id.order_state_indicator);
+        orderedStateTxt = findViewById(R.id.order_state_text);
+        orderedState = findViewById(R.id.ordered_state);
+        cookingState = findViewById(R.id.cooking_state);
+        dispatchedState = findViewById(R.id.dispatched_state);
+        onWayState = findViewById(R.id.on_way_state);
+        deliveredState = findViewById(R.id.delivered_state);
+
+        orderItemRV = findViewById(R.id.ordered_item_rv);
+        orderItemRV.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        orderItemRV.setHasFixedSize(true);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseFirestore.getInstance().collection("orders").document(orderId).addSnapshotListener((value, error) -> {
+            if (value != null && value.exists()) {
+                Order order = value.toObject(Order.class);
+
+                assert order != null;
+                CheckoutCartItemRecyclerAdapter adapter = new CheckoutCartItemRecyclerAdapter(createOrderItemList(order.getItems()));
+                orderItemRV.setAdapter(adapter);
+
+                String tcp = "\u20B9 " + order.getTotalPrice();
+                totalCartPriceTxt.setText(tcp);
+
+                String dp_ = "\u20B9 " + order.getDeliveryPrice();
+                deliveryCartPriceTxt.setText(dp_);
+
+                String tp_ = "\u20B9 " + order.getPayablePrice();
+                totalPayableTxt.setText(tp_);
+
+                String c_ = order.getItems().size() + " Food Items";
+                itemsTxt.setText(c_);
+
+                String o_ = "#~order~" + order.getOrderNumber();
+                orderNumberTxt.setText(o_);
+
+                String t_ = DateParser.parse(new Date(order.getTime()));
+                timeTxt.setText(t_);
+
+                paymentMethodTxt.setText(order.getPaymentMethod());
+                nameTxt.setText(order.getName());
+                addressTxt.setText(order.getAddress());
+                phoneTxt.setText(order.getPhone());
+                orderedStateTxt.setText(getOrderStateTxt(order.getOrderState()));
+
+                if (order.getOrderState() > 1) {
+                    deliveryAgentDesk.setVisibility(View.VISIBLE);
+
+                    agentNameTxt.setText(order.getAgentName());
+                    agentPhoneTxt.setText(order.getAgentPhone());
+                }
+
+                switch (order.getOrderState()) {
+                    case 0:
+                        orderStateIndicator.setProgressCompat(0, true);
+                        orderedState.getBackground().setTint(getColor(R.color.red));
+                        break;
+                    case 1:
+                        orderStateIndicator.setProgressCompat(25, true);
+                        orderedState.getBackground().setTint(getColor(R.color.red));
+                        cookingState.getBackground().setTint(getColor(R.color.red));
+                        break;
+                    case 2:
+                        orderStateIndicator.setProgressCompat(50, true);
+                        orderedState.getBackground().setTint(getColor(R.color.red));
+                        cookingState.getBackground().setTint(getColor(R.color.red));
+                        dispatchedState.getBackground().setTint(getColor(R.color.red));
+                        break;
+                    case 3:
+                        orderStateIndicator.setProgressCompat(75, true);
+                        orderedState.getBackground().setTint(getColor(R.color.red));
+                        cookingState.getBackground().setTint(getColor(R.color.red));
+                        dispatchedState.getBackground().setTint(getColor(R.color.red));
+                        onWayState.getBackground().setTint(getColor(R.color.red));
+                        break;
+                    case 4:
+                        orderStateIndicator.setProgressCompat(100, true);
+                        orderedState.getBackground().setTint(getColor(R.color.red));
+                        cookingState.getBackground().setTint(getColor(R.color.red));
+                        dispatchedState.getBackground().setTint(getColor(R.color.red));
+                        onWayState.getBackground().setTint(getColor(R.color.red));
+                        deliveredState.getBackground().setTint(getColor(R.color.red));
+                        deliveryAgentDesk.setVisibility(View.GONE);
+                        cancelOrderBtn.setVisibility(View.INVISIBLE);
+                        cancelOrderBtn.setEnabled(false);
+                        break;
+                }
+
+                if (!order.isOrderSuccess()) {
+                    orderStateIndicator.setProgressCompat(0, true);
+                    String cancelMsg = "Order was Canceled.";
+                    cancelOrderBtn.setText(cancelMsg);
+                    cancelOrderBtn.setEnabled(false);
+                    orderedStateTxt.setText(cancelMsg);
+                    deliveryAgentDesk.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        acceptOrderBtn.setOnClickListener(view -> {
+
+        });
+
+        cancelOrderBtn.setOnClickListener(view -> {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Order Cancel");
+            alert.setMessage("Are you sure, you want to cancel this order.");
+            alert.setCancelable(true);
+            alert.setPositiveButton("Yes", (dialogInterface, i) -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("orderSuccess", false);
+                FirebaseFirestore.getInstance().collection("orders").document(orderId).update(map)
+                        .addOnSuccessListener(unused -> Toast.makeText(this, "Order was canceled.", Toast.LENGTH_SHORT).show())
+                        .addOnFailureListener(e -> Toast.makeText(this, "Unable to cancel Order.", Toast.LENGTH_SHORT).show());
+            });
+            alert.setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss());
+            alert.show();
+        });
+
+        closeBtn.setOnClickListener(view -> finish());
+    }
+
+    private List<CheckoutCartItem> createOrderItemList(List<CartItem> items) {
+        List<CheckoutCartItem> checkoutCartItems = new ArrayList<>();
+        for (CartItem cartItem : items) {
+            String orderName = cartItem.getFood_data().getName();
+            if (!cartItem.getTopping_ids().equals("None")) {
+                orderName = orderName + " + Toppings";
+            }
+            CheckoutCartItem item = new CheckoutCartItem(orderName, cartItem.getTotal_price());
+            checkoutCartItems.add(item);
+        }
+        return checkoutCartItems;
+    }
+
+    private String getOrderStateTxt(int state) {
+        switch (state) {
+            case 0: return  "Ordered...";
+            case 1: return  "Cooking...";
+            case 2: return  "Dispatched...";
+            case 3: return  "On way...";
+            case 4: return  "Delivered...";
+            default: return "";
+        }
+    }
+}
--- app/src/main/java/com/rotiking/admin/tabs/OrdersFragment.java
+++ app/src/main/java/com/rotiking/admin/tabs/OrdersFragment.java
@@ -3,64 +3,50 @@ package com.rotiking.admin.tabs;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.rotiking.admin.R;
+import com.rotiking.admin.adapter.OrderRecyclerAdapter;
+import com.rotiking.admin.models.Order;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OrdersFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OrdersFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OrdersFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OrdersFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OrdersFragment newInstance(String param1, String param2) {
-        OrdersFragment fragment = new OrdersFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RecyclerView ordersRV;
+    private LinearLayout noOrdersI;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_orders, container, false);
+
+        ordersRV = view.findViewById(R.id.order_rv);
+        ordersRV.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        ordersRV.setHasFixedSize(true);
+
+        noOrdersI = view.findViewById(R.id.no_orders_i);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Query query = FirebaseFirestore.getInstance().collection("orders").orderBy("time", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<Order> options = new FirestoreRecyclerOptions.Builder<Order>().setQuery(query, Order.class).build();
+        OrderRecyclerAdapter adapter = new OrderRecyclerAdapter(options, noOrdersI);
+        ordersRV.setAdapter(adapter);
+        adapter.startListening();
     }
 }
